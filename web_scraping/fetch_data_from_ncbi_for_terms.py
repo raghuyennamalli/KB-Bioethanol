5413bdc0a2c2c4206d5057b2a3782ce88718fa24
PulkiD <pulkitanupam@gmail.com>
2021-10-25 16:44:16 +0200

    commented out makedirs function to avoid unnecessary creation of dirs

--- web_scraping/fetch_data_from_ncbi_for_terms.py
+++ web_scraping/fetch_data_from_ncbi_for_terms.py
@@ -4,7 +4,7 @@ if __name__ == '__main__':
     list_term = ["Casey S Greene","Jayashree Ramana"]
     for term in list_term:
         str_dir_name = os.path.join("../data/",term.replace(" ","_"))
-        os.makedirs(str_dir_name, exist_ok=True)
+        #os.makedirs(str_dir_name, exist_ok=True)
         bool_response = utils.get_data_from_ncbi(str_term=term,
                                                  str_db_name="pubmed",
                                                  str_output_path=str_dir_name)
