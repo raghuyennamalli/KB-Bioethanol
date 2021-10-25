7663d9554e953ebcd50cddf7fd8f15eb5f6b6183
PulkiD <pulkitanupam@gmail.com>
2021-10-25 16:36:39 +0200

    Added makerdirs function if the hits or int_count are above 0

--- web_scraping/utils_webscraping.py
+++ web_scraping/utils_webscraping.py
@@ -22,7 +22,8 @@ def get_data_from_ncbi(str_term=None,str_db_name=None,int_retmax=9900,str_output
     """
     Description: To search in NCBI portal using esearch API for provided term in the provided database
     Input:
-        str_term: query to be searched in NCBI portal
+        str_term: query to be searched in NCBI portal. For two terms follow below pattern:
+                  EGFR AND Breast Cancer
         str_db_nam: name of the NCBI database where query has to be searched
         int_retmax: total number of documents to be retrieved in one batch. Should be less than 10,000.
         str_output_path: path to a directory where output will be stored in xml file format
@@ -34,7 +35,7 @@ def get_data_from_ncbi(str_term=None,str_db_name=None,int_retmax=9900,str_output
         print("Input parameters were not passed as expected by the function. Function will exit !")
         return False
     # format term query: replace spaces with "+"; convert string to lowercase
-    str_term = str_term.replace(" ","+").lower()
+    str_term = str_term.replace(" ","+").lower
     # form esearch url using base url and query
     str_esearch_url = str_ncbi_baseurl+"esearch.fcgi?db={}&term={}&retmax={}&usehistory=y&api_key={}".format(str_db_name,str_term,int_retmax,str_ncbi_apikey)
     # get response of esearch url
@@ -49,6 +50,8 @@ def get_data_from_ncbi(str_term=None,str_db_name=None,int_retmax=9900,str_output
     str_webenv = bs_esearch_response.find("webenv").get_text()
     str_querykey = bs_esearch_response.find("querykey").get_text()
     int_count = int(bs_esearch_response.find("count").get_text())
+    if int_count > 0:
+        os.makedirs(str_output_path, exist_ok=True)
     int_retstart = int(bs_esearch_response.find("retstart").get_text())
     print("Total number of hits for the term {} is: {}".format(str_term,int_count))
     # iterate till int_count and fetch data in batches using retmax
